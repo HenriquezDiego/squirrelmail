d054e9c4c1af8a1619de3c657d921ee6b8330c00
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-14 21:53:02 -0600

    class

---
+++ class/mime/SMimeMessage.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * SMimeMessage.class.php
+ *
+ * This file contains class needed to handle SMIME mime messages.
+ *
+ * @link http://www.ietf.org/html.charters/smime-charter.html
+ * @copyright 2003-2022 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id: SMimeMessage.class.php 14937 2022-01-18 08:02:40Z pdontthink $
+ * @package squirrelmail
+ * @subpackage mime
+ * @since 1.3.2
+ */
+
+/**
+ * Unimplemented class.
+ * @package squirrelmail
+ * @subpackage mime
+ * @todo implement smime parsing
+ */
+class SMimeMessage {
+
+}
+
