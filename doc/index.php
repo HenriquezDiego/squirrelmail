0a9b62f5f910579dd940c947d08fe5e461ac04bb
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-14 21:54:00 -0600

    doc

---
+++ doc/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * index.php
+ *
+ * Redirects to the index.html file.
+ *
+ * @copyright 1999-2022 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id: index.php 14937 2022-01-18 08:02:40Z pdontthink $
+ * @package squirrelmail
+ */
+
+header('Location: index.html');
+
