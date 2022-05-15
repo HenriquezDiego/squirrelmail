a75577b3b2071ce94d19c83d7502f5d3f6fdd8c7
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-14 21:53:14 -0600

    config

---
+++ config/config_local.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Local config overrides.
+ *
+ * You can override the config.php settings here.
+ * Don't do it unless you know what you're doing.
+ * Use standard PHP syntax, see config.php for examples.
+ *
+ * @copyright 2002-2022 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id: config_local.php 14937 2022-01-18 08:02:40Z pdontthink $
+ * @package squirrelmail
+ * @subpackage config
+ */
+
